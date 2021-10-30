ff77c244f9c999be6ee8c7ce125590b508313407
Madhur Maurya <madhurmaurya365@gmail.com>
2021-10-30 11:07:22 +0530

    Splash Screen added

--- aasha/lib/main.dart
+++ aasha/lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:aasha/register.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark), home: Register()));
+  runApp(
+    const AppWidget()
+  );
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFF292929), brightness: Brightness.dark, textTheme: GoogleFonts.montserratTextTheme(Theme.of(context).textTheme)),
+      home: FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 5)),
+        builder: (context, snap) {
+          if(snap.connectionState == ConnectionState.waiting){
+            return const SplashScreen();
+          } else {
+            return const Register();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height/2-200,),
+            Image.asset('assets/icon-fg.png', width: MediaQuery.of(context).size.width/2,),
+            Text('AASHA', style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white70),),
+            const Expanded(child: Center(child: CircularProgressIndicator(color: Color(0xFF7CFDF2),),),),
+            Text('FROM',style: Theme.of(context).textTheme.overline?.copyWith(color: Colors.white70)),
+            Text('Team Omae Wa Mou Shindeiru',style: Theme.of(context).textTheme.overline?.copyWith(color: Colors.white70)),
+            const SizedBox(height: 32,),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- aasha/lib/register.dart
+++ aasha/lib/register.dart
@@ -28,156 +28,109 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(41, 41, 41, 1),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50.0),
+      body: Stack(
+        children: [
+          Positioned( bottom: -150,  right: -150, child: Opacity(opacity: 0.5, child: Image.asset('assets/icon-fg.png'))),
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text(
-                    "Hello!",
-                    style: TextStyle(
-                        color: Color.fromRGBO(185, 152, 255, 1),
-                        fontSize: 35,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Signup to",
-                    style: TextStyle(
-                        color: Color.fromRGBO(185, 152, 255, 1),
-                        fontSize: 35,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "Vote",
-                    style: TextStyle(
-                        color: Color.fromRGBO(185, 152, 255, 1),
-                        fontSize: 35,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 2.2 / 3,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black38,
-                        blurRadius: 25,
-                        offset: const Offset(0, 10),
-                      ),
-                    ]),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Name',
-                    hintStyle: TextStyle(color: Colors.black54),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.5),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(color: const Color(0xFFB998FF)),
+                        ),
+                        Text(
+                          "Sign-up to vote",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3
+                              ?.copyWith(color: const Color(0xFFB998FF)),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 2.2 / 3,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.5),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black38,
-                          blurRadius: 25,
-                          offset: const Offset(0, 10),
-                        ),
-                      ]),
-                  child: TextFormField(
+                  TextFormField(
+                    style: const TextStyle(color: Colors.white),
                     decoration: InputDecoration(
-                      hintText: 'Aadhar Number',
-                      hintStyle: TextStyle(color: Colors.black54),
+                      hintText: 'Name',
                       filled: true,
-                      fillColor: Colors.white.withOpacity(0.5),
+                      fillColor: Colors.black12,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
                         borderSide: BorderSide.none,
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 2.2 / 3,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.5),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black38,
-                          blurRadius: 25,
-                          offset: const Offset(0, 10),
-                        ),
-                      ]),
-                  child: Container(
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Aadhar Number',
+                      filled: true,
+                      fillColor: Colors.black12,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Colors.white.withOpacity(0.5),
+                      color: Colors.black12,
                     ),
                     child: ListTile(
-                      leading: Icon(
+                      leading: const Icon(
                         Icons.event,
-                        color: Colors.black54,
                       ),
                       title: Text(
                         '${selectedDate.day}/${selectedDate.month}/${selectedDate.year}',
-                        style: TextStyle(color: Colors.black54),
+                        style: const TextStyle(color: Colors.white),
                       ),
                       onTap: _pickdate,
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 2.2 / 3,
+                      height: 48,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xFFB998FF)),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Sign-up",
+                          style:
+                              TextStyle(color: Colors.white.withOpacity(0.8)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 2.2 / 3,
-                height: 48,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Color.fromRGBO(185, 152, 255, 1)),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "SignUp",
-                    style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white.withOpacity(0.8)),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
+          ),
+          
+        ],
       ),
     );
   }
