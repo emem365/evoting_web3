77f20c722c4a8b2900eaef148635103d278f0364
aishuo07 <aish.kanodia@netmeds.com>
2021-10-30 18:46:01 +0530

    formatting

--- aasha/backend/backend.py
+++ aasha/backend/backend.py
@@ -15,12 +15,21 @@ def replace_file(name, dob, aadhar, pub_address):
     doc.render(context)
     doc.save('result2.docx')
 
+# connect to the ipfs client
 res = ipfshttpclient.Client('/ip4/127.0.0.1/tcp/5001')
+
+#flask app
 app = Flask(__name__)
+
+
 @app.route('/', methods = ['POST'])
 def addfiletoipfs():
-    body = request.json
-    replace_file(body['name'], body['dob'], body['aadhar'], body['pub_address'])
-    hash = res.add(r'result2.docx')
-    return jsonify(hash), 200
-app.run(port=8000)
+    try:
+        body = request.json
+        replace_file(body['name'], body['dob'], body['aadhar'], body['pub_address'])
+        hash = res.add(r'result2.docx')
+        return jsonify(hash), 200
+    except:
+        return jsonify({"error": "error"}), 400
+if __name__ == "__main__":
+    app.run(port=8000)
